1fc2b913941fa71489cfeb193c4c39594131be87
ru dian <913049907@qq.com>
2016-10-10 09:49:11 +0800

    添加API类

---
+++ LaunchModel/API.h
@@ -0,0 +1,14 @@
+//
+//  API.h
+//  LaunchModel
+//
+//  Created by ru dian on 16/10/9.
+//  Copyright © 2016年 尚学. All rights reserved.
+//
+//说明：该声明文件用于查询接口等
+
+#ifndef API_h
+#define API_h
+
+
+#endif /* API_h */
